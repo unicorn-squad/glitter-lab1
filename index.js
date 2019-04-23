61123dee82b5a533735c1b5dafe943cf1ee224e8
aramirezdecruz3148 <leigh3148@gmail.com>
2019-04-23 13:32:52 -0700

    we switched some file locations and started tdd set-up

---
+++ index.js
@@ -0,0 +1,6 @@
+const questionOne = document.getElementById('question-one');
+const questionTwo = document.getElementById('question-two');
+const questionThree = document.getElementById('question-three');
+const questionFour = document.getElementById('question-four');
+const questionFive = document.getElementById('question-five');
+const submitButton = document.getElementById('submit-button');
